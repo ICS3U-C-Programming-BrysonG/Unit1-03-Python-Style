1ab20d4f3a7aceb427c36feb78d5a4ace3a80380
Bryson Grant <Bryson.grant@stu.ocsb.ca>
2026-09-15 12:19:14 +0000

    First commit of hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Created By: Bryson
+# Date: September 14th, 2026
+# This program displays a greeting, and contains proper formatting.
+def main():
+    print("Hello, Ms.Raffin")
+    print("Programming is so fun!")
+
+
+if __name__ == "__main__":
+    main()
